c4302b54c98908f41bc93b2348706bff9c963977
Bayron Fajardo <bayronfajardor@gmail.com>
2025-10-28 00:33:13 -0500

    login sencillo

--- gym_tracker/urls.py
+++ gym_tracker/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-   path('login/', auth_views.LoginView.as_view(template_name = "login.html"), name="login"),
-   path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+   path('', home),
 ]
